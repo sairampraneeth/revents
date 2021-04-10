865e23d5589be6b71e3bb81fb997b776de987a22
Sairam Praneeth Vegesana <sairampraneeth@gmail.com>
2021-04-09 21:40:05 -0400

    End of section 1

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './app/layout/styles.css';
+import App from './app/layout/app.jsx';
+import reportWebVitals from './reportWebVitals';
+import { render } from '@testing-library/react';
+
+const rootEL = document.getElementById('root');
+
+function Render() {
+  ReactDOM.render(
+    <App />,
+    rootEL
+);
+}
+
+if (module.hot){
+  module.hot.accept('./app/layout/app.jsx', function(){
+    setTimeout(Render);
+  })
+}
+
+Render();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
