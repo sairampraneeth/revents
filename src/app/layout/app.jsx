386d246f932ea560f85b93b7d86c442ed6f80516
Sairam Praneeth Vegesana <sairampraneeth@gmail.com>
2021-04-09 23:33:28 -0400

    temporary save

--- src/app/layout/app.jsx
+++ src/app/layout/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 function App() {
+  const title = React.createElement('h1', {}, 'Revents no JSX');
+
+  const div = React.createElement('div', {className: 'App'}, title);
+
   return (
-    <div className="App">
-      <h1>Re-vents</h1>
-    </div>
+    div
   );
 }
 
